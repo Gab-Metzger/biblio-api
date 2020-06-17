12ddb08ac8be632d33d1a67c9a3fbbcf05745325
Gabriel Metzger <gab.metzger@gmail.com>
2020-06-17 20:31:31 +0200

    fix: test env file existence before loading

--- bootstrap.php
+++ bootstrap.php
@@ -4,9 +4,9 @@ require 'vendor/autoload.php';
 use Dotenv\Dotenv;
 use Src\System\DatabaseConnector;
 
-
-$dotenv = new DotEnv(__DIR__);
-$dotenv->load();
-
+if (file_exists(__DIR__ . '/.env')) {
+  $dotenv = new DotEnv(__DIR__);
+  $dotenv->load();
+}
 
 $dbConnection = (new DatabaseConnector())->getConnection();
